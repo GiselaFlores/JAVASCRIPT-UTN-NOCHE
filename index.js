7899717696ab0e6ae5a42967080d7ab354743bb2
GiselaFlores <gi.flores@bue.edu.ar>
2024-06-11 21:16:20 -0300

    Variables

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("Hola Mundo JS");
+
+// comentario
+
+/* comentario de bloque */
+
+let nombre = "Gise";
+let saludo, numero = "10", numeroDos = 5;
+
+//var nombreDos = "Perez";
+
+nombre = "Flores";
+console.log(nombre);
+console.log("Hola bienvenida: " + nombre);
+
+const PI = true;
+console.log(PI*2);
+
+let objeto = {
+    nombre: "Gise",
+    edad: 43,
+    cargo: "docente",
+    hijos: true,
+    trabajos: ["unlam", "utn", "codo a codo", "tecnicatura software"]
+}
+
+console.log(objeto.hijos);
+console.log(objeto.trabajos[2]);
+console.log(typeof objeto);
+
+const num = 4;
+const num2 = new Number(4);
+console.log(typeof num2);
+console.log({num});
