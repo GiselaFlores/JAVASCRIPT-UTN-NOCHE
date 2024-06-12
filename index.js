91aaf94a30d66b819948fda61d41b9a1816b3bb9
GiselaFlores <gi.flores@bue.edu.ar>
2024-06-12 10:44:04 -0300

    intro JS

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-console.log("Hola Mundo JS");
+console.log("Hola JS desde hoja externa");
 
-// comentario
-
-/* comentario de bloque */
+// comentarios en linea
+/* comentarios de bloque */
 
 let nombre = "Gise";
-let saludo, numero = "10", numeroDos = 5;
+console.log(nombre);
 
-//var nombreDos = "Perez";
+let saludo = true, numero ="10", numeroDos = 5;
+var nombreDos = "Perez";
 
 nombre = "Flores";
 console.log(nombre);
-console.log("Hola bienvenida: " + nombre);
+console.log(nombre + saludo);
 
-const PI = true;
-console.log(PI*2);
+let cadena = new String("hola");
 
 let objeto = {
     nombre: "Gise",
     edad: 43,
     cargo: "docente",
     hijos: true,
-    trabajos: ["unlam", "utn", "codo a codo", "tecnicatura software"]
+    trabajo: ["unlam", "utn", "codo a codo", "tecnicatura de software"]
 }
 
-console.log(objeto.hijos);
-console.log(objeto.trabajos[2]);
-console.log(typeof objeto);
+console.log(objeto.trabajo[2]);
+console.log({objeto});
 
-const num = 4;
-const num2 = new Number(4);
+console.log(typeof cadena);
+
+let num = 4;
+let num2 = new Number(4);
 console.log(typeof num2);
 console.log({num});
+
+const PI = 3.141825;
+console.log(PI*2*numeroDos);
+
+alert("vas a ser bloqueado");
+let edad = parseInt(prompt("Ingrese su edad"));
+console.log(edad*3);
+
